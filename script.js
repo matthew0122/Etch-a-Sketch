26fa6bc33daa570aa5c8790b87a2fc575574c43a
Matthew Bergin <matthewdbergin@gmail.com>
2023-03-06 15:00:27 -0500

    add hover event

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
+let rows = 16;
 
-for(let i = 0; i < 16; i++){
+for(let i = 0; i < rows; i++){
     const flex = document.createElement('div');
     flex.classList.add('flexy');
-    for(let j = 0; j < 16; j++){
+    for(let j = 0; j < rows; j++){
         const square = document.createElement('div');
         square.classList.add("square");
+        square.addEventListener("mouseover", color);
         flex.append(square);
     }
     document.body.append(flex);
+}
+
+function color(e){
+    this.classList.add("hovered");
 }
