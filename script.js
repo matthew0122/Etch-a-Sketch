8a1e2db7b266bc3b2c0c62c83003b5bf27e68fc5
Matthew Bergin <matthewdbergin@gmail.com>
2023-03-06 15:23:51 -0500

    Change amount of rows and columns

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
-let rows = 16;
+let row = 16;
 const amtChanger = document.getElementById("amount");
 amtChanger.addEventListener("click", givePrompt);
-for(let i = 0; i < rows; i++){
-    const flex = document.createElement('div');
-    flex.classList.add('flexy');
-    for(let j = 0; j < rows; j++){
-        const square = document.createElement('div');
-        square.classList.add("square");
-        square.addEventListener("mouseover", color);
-        flex.append(square);
-    }
-    document.body.append(flex);
-}
+addDivs(16);
 
 function color(e){
     this.classList.add("hovered");
@@ -19,6 +9,7 @@ function color(e){
 function givePrompt(e){
     let rows = prompt("How many squares would you like per row and column?");
     removeDivs();
+    addDivs(rows);
 } //ADD INPUT VALIDATION
 function removeDivs() {
     const row = document.getElementsByClassName("flexy");
@@ -26,5 +17,20 @@ function removeDivs() {
     row2.forEach(element => {
         document.body.removeChild(element);
     });
-    console.log("hi");
+}
+function addDivs(rows){
+    const width = 960/rows;
+    for(let i = 0; i < rows; i++){
+        const flex = document.createElement('div');
+        flex.classList.add('flexy');
+        for(let j = 0; j < rows; j++){
+            const square = document.createElement('div');
+            square.classList.add("square");
+            square.style.width = width + "px";
+            square.style.height = width+"px";
+            square.addEventListener("mouseover", color);
+            flex.append(square);
+        }
+        document.body.append(flex);
+    }
 }
